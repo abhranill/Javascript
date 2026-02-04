e67359a3f2d9ce708e70440a796626f8f7505b5e
Abhranil Roy <abhranil.roy07@gmail.com>
2026-02-04 23:42:23 +0530

    variables

--- new.js
+++ new.js
@@ -1,1 +1,5 @@
  console.log("code is running")
+ var a=4;
+ var b=5;
+ console.log(a+b)
+ close.log(typeof a)
---
+++ vid55 js/learnn.js
@@ -0,0 +1,29 @@
+console.log("hello")
+let a=4;
+let b=5;
+console.log(a+b)
+console.log(typeof a)
+console.log(a+b+8)
+
+{
+    let a=66;
+    console.log(a)
+}
+console.log(a)
+
+
+
+let x="abhranil";
+let y=22;
+let z= 3.55;
+const p= true;
+let q= undefined;
+let r= null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+let o={
+    name: "abhranil",
+    "job role": 5600,
+}
+console.log(o)
